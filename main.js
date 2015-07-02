10a99ddd483e48f391b2368e36643daf72607be1
chloelangston <chloealangston@gmail.com>
2015-07-01 19:17:10 -0700

    applied css elements to new items

--- main.js
+++ main.js
@@ -16,6 +16,12 @@ $(
     var template = '<p><input type="checkbox"><i class="glyphicon glyphicon-star"></i><span>' + text + '</span><i class="glyphicon glyphicon-remove"></i></p>';
     console.log(text);
     $('.list').append(template);
+    $('.glyphicon-star').click(function() {
+    $(this).css('color', '#ffcc00');
+  });
+  $('.glyphicon-remove').click(function() {
+    $(this).parent().remove();
+  });
   });
 
 });
