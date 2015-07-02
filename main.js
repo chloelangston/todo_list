d7517a686e1ac6e587fd1f85050992ef753b00cb
chloelangston <chloealangston@gmail.com>
2015-07-01 18:18:24 -0700

    created to do list functions in jquery

---
+++ main.js
@@ -0,0 +1,21 @@
+$(
+  function(){
+    $('.glyphicon-star').click(function() {
+    $(this).css('color', '#ffcc00');
+  });
+
+  $('.glyphicon-remove').click(function() {
+    $(this).parent().remove();
+  });
+
+
+
+  $('button.btn').click(function(e) {
+    e.preventDefault();
+    var text = $("#todo").val();
+    var template = '<p><input type="checkbox"><i class="glyphicon glyphicon-star"></i><span>' + text + '</span><i class="glyphicon glyphicon-remove"></i></p>';
+    console.log(text);
+    $('.list').append(template);
+  });
+
+});
